50ddcc19a2a249524dbcf81333c7da62a73123a0
Ishwari <ishwari.walke1112@gmail.com>
2026-01-31 14:49:35 +0530

    add colour theme inside extend in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+darkMode: 'class',
+content: ['./index.html', './src/**/*.{js,jsx}'],
+theme: {
+extend: {
+colors: {
+primary: '#6E605A', // extracted from your theme image (can tweak later)
+secondary: '#A89C95',
+accent: '#E6DDD8',
+},
+},
+},
+plugins: [],
+};
