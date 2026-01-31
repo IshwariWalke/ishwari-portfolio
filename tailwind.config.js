50165535da58bd9cc357f21c2cddda2792a70be8
Ishwari <ishwari.walke1112@gmail.com>
2026-02-01 01:23:37 +0530

    include Inter font family inside extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 export default {
-darkMode: 'class',
-content: ['./index.html', './src/**/*.{js,jsx}'],
-theme: {
-extend: {
-colors: {
-primary: '#6E605A', // extracted from your theme image (can tweak later)
-secondary: '#A89C95',
-accent: '#E6DDD8',
-},
-},
-},
-plugins: [],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+         primary: '#6E605A',
+        secondary: '#A89C95',
+        accent: '#E6DDD8',
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
 };
